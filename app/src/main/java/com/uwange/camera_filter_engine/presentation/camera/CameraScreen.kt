39aae93f24e22598a6d3f7a923a3eb0ab115c4da
U-WangE <chamchann@gmail.com>
2026-03-28 14:33:55 +0900

    feat:CameraX 프로뷰 추가

--- app/src/main/java/com/uwange/camera_filter_engine/presentation/camera/CameraScreen.kt
+++ app/src/main/java/com/uwange/camera_filter_engine/presentation/camera/CameraScreen.kt
@@ -5,16 +5,24 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
@@ -116,5 +124,36 @@ fun CameraScreen(
 private fun CameraPreview(
     modifier: Modifier = Modifier,
 ) {
-    Box(modifier = modifier.fillMaxSize())
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val previewView = remember {
+        PreviewView(context)
+    }
+    val preview = remember {
+        Preview.Builder().build()
+    }
+
+    DisposableEffect(lifecycleOwner, context) {
+        val cameraProvider = ProcessCameraProvider.getInstance(context).get()
+
+        preview.surfaceProvider = previewView.surfaceProvider
+
+        val selector = CameraSelector.DEFAULT_BACK_CAMERA
+
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(
+            lifecycleOwner,
+            selector,
+            preview
+        )
+
+        onDispose {
+            cameraProvider.unbindAll()
+        }
+    }
+
+    AndroidView(
+        factory = { previewView },
+        modifier = modifier
+    )
 }
